cb1ad664c59282292cff313e70302cdcf6be7ae0
Aina <ainamariamonserratsanchez@alumnes.iesllucmajor.org>
2023-05-09 12:52:37 +0200

    Primera versió tasca 4 GitHub

---
+++ Tasca4GitHubExercici1/src/main/java/com/mycompany/tasca4githubexercici1/Tasca4GitHubExercici1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tasca4githubexercici1;
+
+/**
+ *
+ * @author Aina
+ */
+public class Tasca4GitHubExercici1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
